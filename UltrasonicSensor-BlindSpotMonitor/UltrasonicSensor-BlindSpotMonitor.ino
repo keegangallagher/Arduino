d1ffd5d2bca0435da610dae712491459fef5fae4
Keegan G <keegangallagher2006@gmail.com>
2023-03-16 14:08:32 -0400

    Initial Blind Spot Monitor Commit
    Stable

---
+++ UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
@@ -0,0 +1,18 @@
+//Programmer: Keegan Gallagher
+//Program: Blind Spot Monitor
+//Date: 03.16.2023
+
+/*
+ * This program will determine distances approaching
+ * vehicle that are in your blind spot and turn on warning sign
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
