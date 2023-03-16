74df7134b46f5656d5f3b4f72338f6b65fedce80
Keegan G <keegangallagher2006@gmail.com>
2023-03-16 14:13:56 -0400

    Added Resource
    stable

--- UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
+++ UltrasonicSensor-BlindSpotMonitor/UltrasonicSensor-BlindSpotMonitor.ino
@@ -1,6 +1,7 @@
 //Programmer: Keegan Gallagher
 //Program: Blind Spot Monitor
 //Date: 03.16.2023
+//Resource: https://projecthub.arduino.cc/Krepak/a6ea3ad5-fd23-49c9-a96c-5e6fcf97f2cc?ref=search&ref_id=red%20green%20yellow%20light%20echo%20sensor&offset=1
 
 /*
  * This program will determine distances approaching
